70d783723b4e2853e5e758af46b875b9cdecbf5a
m-nazhat-adiba <140298565+m-nazhat-adiba@users.noreply.github.com>
2023-09-22 17:18:21 +0700

    edited layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,15 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import Layout from "@/components/layout";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    ></main>
+      className={`flex min-h-screen flex-col items-center  ${inter.className}`}
+    >
+      <Layout />
+    </main>
   );
 }
