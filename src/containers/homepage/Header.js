eba7ef1413be47b1fda8dcce1d7e2cf647062184
m-nazhat-adiba <140298565+m-nazhat-adiba@users.noreply.github.com>
2023-09-24 18:11:28 +0700

    edited layout

--- src/containers/homepage/Header.js
+++ src/containers/homepage/Header.js
@@ -1,13 +1,21 @@
 import React from "react";
 import Image from "next/image";
+import Banner from "../../../public/Header-2.png";
+import { Icon } from "@iconify/react";
 
 const Header = () => {
   return (
-    <div className="relative flex flex-col w-full h-[740px] items-center justify-end">
-      <div className="flex w-[640px] h-auto">
-        <img src="/Header-2.png" alt="banner" />
+    <div className="relative flex flex-col w-full h-[90vh] items-center justify-end">
+      <div className="flex w-[800px] h-auto">
+        <Image
+          src={Banner}
+          width={1000}
+          height={1100}
+          alt="Banner"
+          priority={false}
+        />
       </div>
-      <div className="absolute flex flex-col left-0 bottom-12 gap-4">
+      <div className="absolute flex flex-col left-0 bottom-14 gap-4">
         <h1 className="w-5 text-5xl text-red-600 font-semibold leading-tight">
           NAZHAT ADIBA
         </h1>
@@ -18,6 +26,18 @@ const Header = () => {
           </p>
         </div>
       </div>
+      <div className="absolute right-0 bottom-[48%] flex flex-col items-end gap-4">
+        <p className="text-gray-500 w-2/3 text-right mr-4">
+          Feel free to check my resume
+        </p>
+        <button className="flex items-center gap-3 px-6 py-2 border-2 w-fit border-gray-500 text-gray-500 rounded-full">
+          <Icon icon="akar-icons:download" className="text-lg" />
+          <p>My Resume</p>
+        </button>
+      </div>
+      <p className="absolute right-0 bottom-14 text-gray-400">
+        PERSONAL PORTFOLIO
+      </p>
     </div>
   );
 };
