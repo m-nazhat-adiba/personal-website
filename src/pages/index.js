940b226e4b9cef909d445b4a8dc5ea07be82b007
m-nazhat-adiba <140298565+m-nazhat-adiba@users.noreply.github.com>
2023-09-26 09:03:11 +0700

    edited layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,14 @@ import Image from "next/image";
 import Layout from "@/components/layout";
 import Header from "@/containers/homepage/Header";
 import About from "@/containers/homepage/About";
+import Works from "@/containers/homepage/Works";
 
 export default function Home() {
   return (
     <Layout>
       <Header />
       <About />
+      <Works />
     </Layout>
   );
 }
