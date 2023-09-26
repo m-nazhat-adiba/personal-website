2ed6ce91f24a0b05e2fb1313481e06859e8d9057
m-nazhat-adiba <140298565+m-nazhat-adiba@users.noreply.github.com>
2023-09-26 09:31:24 +0700

    added Works container into page

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,13 +3,17 @@ import Layout from "@/components/layout";
 import Header from "@/containers/homepage/Header";
 import About from "@/containers/homepage/About";
 import Works from "@/containers/homepage/Works";
+import Skills from "@/containers/homepage/Skills";
 
 export default function Home() {
   return (
     <Layout>
-      <Header />
-      <About />
-      <Works />
+      <div className="flex flex-col gap-24 mb-8">
+        <Header />
+        <About />
+        <Works />
+        <Skills />
+      </div>
     </Layout>
   );
 }
