e3f0390fb97249325f3d4c536bc5301b0100f724
m-nazhat-adiba <140298565+m-nazhat-adiba@users.noreply.github.com>
2023-12-18 15:10:13 +0700

    fixed navbar wrapper bug

--- src/components/common/layout/index.js
+++ src/components/common/layout/index.js
@@ -4,9 +4,9 @@ import Footer from "../footer";
 
 const Layout = ({ children }) => {
   return (
-    <div className="px-4 xl:px-2 w-full">
-      <div className="absolute w-full top-0">
-        <div className="container mx-auto">
+    <div className="w-full">
+      <div className="relative w-full top-0 container mx-auto">
+        <div className="absolute w-full px-4 xl:px-2">
           <NavBar />
         </div>
       </div>
