0285939b264d90e025a444d0a74f0b53b0ff0c44
m-nazhat-adiba <140298565+m-nazhat-adiba@users.noreply.github.com>
2023-12-21 10:55:52 +0700

    updated data

--- src/dummy_data/works.js
+++ src/dummy_data/works.js
@@ -1,6 +1,22 @@
 const worksData = [
   {
     id: 1,
+    src: "/work-the-blog.png",
+    title: "The Blog",
+    desc: "A mock-up of blog website. Delivering cleanliness, good-readability and intuitive user interface. Built of JSON Placeholder",
+    tags: ["React", "Tailwind", "Next.js", "Next UI", "Javascript"],
+    url: "https://11-11-news.vercel.app/",
+  },
+  {
+    id: 2,
+    src: "/work-pokedex.png",
+    title: "Pokedex",
+    desc: "Delivering the most of important details of every Pokemon in the games. Built of PokeAPI",
+    tags: ["React", "Tailwind", "Next.js", "Javascript"],
+    url: "https://sweet-salamander-29eb3e.netlify.app/",
+  },
+  {
+    id: 3,
     src: "/work-mudocafe.png",
     title: "Mudocafe",
     desc: "Mudocafe is a user-friendly food ordering website that offers a various menu, providing detailed information, estimation cooking time and promos.",
@@ -8,20 +24,12 @@ const worksData = [
     url: "https://joyful-monstera-ccea42.netlify.app/",
   },
   {
-    id: 2,
-    src: "/work-pokedex.png",
-    title: "Pokedex",
-    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ultrices lorem non feugiat egestas amet.",
-    tags: ["React", "Tailwind", "Next.js"],
-    url: "https://sweet-salamander-29eb3e.netlify.app/",
-  },
-  {
-    id: 3,
-    src: "/work-the-blog.png",
-    title: "The Blog",
-    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ultrices lorem non feugiat egestas amet.",
-    tags: ["React", "Tailwind", "Next.js", "Next UI"],
-    url: "https://11-11-news.vercel.app/",
+    id: 4,
+    src: "/work-trippin.png",
+    title: "Trippin.admin",
+    desc: "A fully responsive Content Management Service (CMS) web app for a travel service. Delivering a clean and intuitive web app to help an admin do various CRUD tasks of activity, promo etc.",
+    tags: ["React", "Next.js", "Javascript", "Tailwind"],
+    url: "https://trippin-travel-admin-ui-93ir-338wpktjf-m-nazhat-adiba.vercel.app/",
   },
 ];
 
