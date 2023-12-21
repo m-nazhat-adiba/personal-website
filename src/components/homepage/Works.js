2f947f23bc63e455bd764cb3e8176547440ff290
m-nazhat-adiba <140298565+m-nazhat-adiba@users.noreply.github.com>
2023-12-21 10:56:18 +0700

    reversed order of data

--- src/components/homepage/Works.js
+++ src/components/homepage/Works.js
@@ -3,15 +3,17 @@ import worksData from "@/dummy_data/works";
 import WorkCard from "@/components/common/cards/WorkCard";
 
 const Works = () => {
+  const chronologicData = [...worksData].reverse();
+
   return (
     <div className="flex flex-col gap-24 items-center md:items-start">
       <h1 className="text-4xl text-gray-400 font-semibold">
         Selected <span className="text-red-500">Works</span>
       </h1>
       <div className="flex flex-col gap-20">
-        {worksData.map((item, key) => (
-          <WorkCard key={key} data={item} />
-        ))}
+        {chronologicData.map((item, key) =>
+          key < 3 ? <WorkCard key={key} data={item} /> : null
+        )}
       </div>
     </div>
   );
