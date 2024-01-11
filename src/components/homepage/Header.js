0f549d547575842393d6cd30b1eed1f6f16c7381
m-nazhat-adiba <140298565+m-nazhat-adiba@users.noreply.github.com>
2024-01-11 15:29:35 +0700

    removed star anim

--- src/components/homepage/Header.js
+++ src/components/homepage/Header.js
@@ -1,48 +1,23 @@
-import React, { useEffect } from "react";
 import { Icon } from "@iconify/react";
-
 import gsap from "gsap";
+import React, { useEffect } from "react";
 
 const Header = () => {
   return (
-    <div>
-      <div className=" flex flex-col-reverse xl:flex-col w-full h-screen lg:items-center xl:justify-end">
-        <div className="relative w-full h-full overflow-hidden flex justify-center">
-          <div className="absolute top-0 w-full h-full container px-4 xl:px-2 ">
-            <div className="bottom-[44%] absolute flex flex-col md:left-0 lg:bottom-14 gap-4">
-              <h1 className="w-5 md:text-5xl text-4xl text-red-600 font-semibold leading-tight">
-                NAZHAT ADIBA
-              </h1>
-              <div className="flex flex-col gap-1">
-                <p className="text-white font-medium md:text-3xl text-xl">
-                  Frontend Developer
-                </p>
-                <p className="md:text-2xl text-lg text-gray-500 font-medium">
-                  From <span className="text-red-600">IN</span>
-                </p>
-              </div>
-            </div>
-            <div className="top-[18%] right-100 absolute md:right-0 md:top-[36%] xl:top-auto xl:bottom-[48%] flex flex-col xl:items-end gap-4 items-end">
-              <p className="md:block hidden text-gray-500 w-2/3 mr-4 text-right">
-                Feel free to check my resume
-              </p>
-              <button className="group flex items-center gap-3 my-4 xl:my-0 px-6 py-2 border-2 w-fit border-gray-500 text-gray-500 rounded-full duration-200 hover:border-gray-400 hover:scale-110">
-                <Icon
-                  icon="akar-icons:download"
-                  className="md:text-lg text-base group-hover:text-gray-400"
-                />
-                <p className="group-hover:text-gray-400 md:text-base text-sm">
-                  My Resume
-                </p>
-              </button>
-            </div>
-            <p className="xl:block hidden absolute right-0 bottom-14 text-gray-400">
-              PERSONAL PORTFOLIO
-            </p>
-          </div>
+    <>
+      <div className="relative flex flex-col w-full h-screen justify-center items-center">
+        <div className="flex flex-col items-center justify-center gap-3">
+          <h1 className="text-5xl">
+            Hello, I'm <b>Nazhat Adiba</b>
+          </h1>
+          <p className="text-2xl font-light">I'm a front-end developer</p>
+        </div>
+        <div className="absolute bottom-0 w-full items-center justify-center flex flex-col">
+          <p className="text-sm font-light">Look to know what i do</p>
+          <Icon icon="akar-icons:chevron-down" className="h-5 w-5 mb-5 mt-2" />
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
