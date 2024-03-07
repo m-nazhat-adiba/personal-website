48e8007eb6e33f1b48c19f2fc6f0df7df495adb5
Nazhat Adiba <140298565+m-nazhat-adiba@users.noreply.github.com>
2024-03-07 17:00:04 +0700

    Update Header.js
    change &apos;

--- src/components/homepage/Header.js
+++ src/components/homepage/Header.js
@@ -8,9 +8,9 @@ const Header = () => {
       <div className="relative flex flex-col w-full h-screen justify-center items-center">
         <div className="flex flex-col items-center justify-center gap-3">
           <h1 className="text-5xl">
-            Hello, I'm <b>Nazhat Adiba</b>
+            Hello, I&apos;m <b>Nazhat Adiba</b>
           </h1>
-          <p className="text-2xl font-light">I'm a front-end developer</p>
+          <p className="text-2xl font-light">I&apos;m a front-end developer</p>
         </div>
         <div className="absolute bottom-0 w-full items-center justify-center flex flex-col">
           <p className="text-sm font-light">Look to know what i do</p>
